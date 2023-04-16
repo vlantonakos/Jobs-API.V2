270607499f098b1a9e80edf97bec9ad43b1a21a6
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-04-16 12:42:08 +0300

    Add files via upload

---
+++ Jobs API/starter/controllers/jobs.js
@@ -0,0 +1,41 @@
+const Job = require('../models/Job')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError,NotFoundError} = require('../errors')
+
+const getAllJobs = async(req,res) => {
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs,count:jobs.length})
+}
+
+const getJob = async(req,res) => {
+    const {user:{userId},params:{id:jobId}} = req
+    const job = await Job.findOne({
+        _id:jobId,createdBy:userId
+    })
+    if(!job){
+        throw new NotFoundError(`No Job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
+}
+
+const createJob = async(req,res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+}
+
+const updateJob = async(req,res) => {
+    res.send('update job')
+}
+
+const deleteJob = async(req,res) => {
+    res.send('deletejob')
+}
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+}
